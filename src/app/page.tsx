1032c7aa45fe4c6157d4fef39436d4301cf2c93f
Maurice Miot <maurice@MacBook-Pro-de-Maurice.local>
2025-07-08 06:43:27 -0400

    test netlify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import dynamic from "next/dynamic";
-// import SeatsEditor from "./components/SeatsEditor";
-// import SeatMap from "./components/Seats";
-
-const Plans = dynamic(() => import("./components/Plans"), {
-  ssr: false,
-});
-
-const Seats = dynamic(() => import("./components/Seats"), {
-  ssr: false,
-});
+import Plans from "./components/Plans";
+import Seats from "./components/Seats";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      {" "}
       <Plans />
-      {/* <SeatsEditor />  */}
       <Seats />
     </div>
   );
